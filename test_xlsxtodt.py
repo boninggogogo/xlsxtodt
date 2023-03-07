ea976f39842e278b99949e8d5964c9f2b75a0618
Boning <1391478312@qq.com>
2023-03-07 09:35:10 +0800

    Just missing input_acus

--- test_xlsxtodt.py
+++ test_xlsxtodt.py
@@ -1,12 +1,11 @@
 from xlsxtodt import xtd
 
-
-R02_room1 = xtd()
-R02_room1.xlsx_to_dt(path_xlsx=r'data/input/Data Hall Geometry input_R02CA.xlsx',
-                     sheets=['level 2', 'level 3', 'Level 4'],
-                     server_sheets=['Level 2 Server', 'Level 3 Server', 'Level 4 Server'],
-                     server_random_number=[10, 10, 10],
-                     types='geometryModel',
-                     output_filename=['Level_2_R02.json', 'Level_3_R02.json', 'Level_4_R02.json'],
-                     name_in_dt=['Level_2_R02_v1', 'Level_3_R02_v1', 'Level_4_R02_v1']
-                     )
+R02_room = xtd()
+R02_room.xlsx_to_dt(path_xlsx=r'data/input/Data Hall Geometry input_R02CA.xlsx',
+                    sheets=['level 2', 'level 3', 'Level 4'],
+                    server_sheets=['Level 2 Server', 'Level 3 Server', 'Level 4 Server'],
+                    server_random_number=[10, 10, 10],
+                    types='geometryModel',
+                    output_filename=['Level_2_R02.json', 'Level_3_R02.json', 'Level_4_R02.json'],
+                    name_in_dt=['Level_2_R02_v1', 'Level_3_R02_v1', 'Level_4_R02_v1']
+                    )
